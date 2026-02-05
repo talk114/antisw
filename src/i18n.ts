b5c9ce46900e8282c8ea1af8636121a4cc5d518a
Hieu <0043113@gmail.com>
2026-02-05 11:26:13 +0700

    submit d

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,17 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./locales/en.json";
-import zh from "./locales/zh.json";
-import zhTW from "./locales/zh-TW.json";
-import ja from "./locales/ja.json";
-import tr from "./locales/tr.json";
 import vi from "./locales/vi.json";
-import pt from "./locales/pt.json";
-import ru from "./locales/ru.json";
-import ko from "./locales/ko.json";
-import ar from "./locales/ar.json";
-import es from "./locales/es.json";
-import my from "./locales/my.json";
 
 i18n
     // detect user language
@@ -28,62 +18,14 @@ i18n
             en: {
                 translation: en,
             },
-            zh: {
-                translation: zh,
-            },
-            "zh-TW": {
-                translation: zhTW,
-            },
-            ja: {
-                translation: ja,
-            },
-            tr: {
-                translation: tr,
-            },
-            // Handling 'zh-CN' as 'zh'
-            "zh-CN": {
-                translation: zh,
-            },
+           
             vi: {
                 translation: vi,
             },
             "vi-VN": {
                 translation: vi,
             },
-            pt: {
-                translation: pt,
-            },
-            "pt-BR": {
-                translation: pt,
-            },
-            ru: {
-                translation: ru,
-            },
-            ko: {
-                translation: ko,
-            },
-            ar: {
-                translation: ar,
-            },
-            es: {
-                translation: es,
-            },
-            "es-ES": {
-                translation: es,
-            },
-            "es-MX": {
-                translation: es,
-            },
-            my: {
-                translation: my,
-            },
-            "ms": {
-                translation: my,
-            },
-            "ms-MY": {
-                translation: my,
-            },
-        },
+                  },
         fallbackLng: "en",
         debug: false, // Set to true for development
 
